c436d067ba7b8a43ede45441c594e8ef391e97e1
bennetthilberg <bennett.hilberg@gmail.com>
2023-11-02 15:07:22 -0500

    Complete FindNeighbor

--- NFAnimate.cpp
+++ NFAnimate.cpp
@@ -4,11 +4,46 @@
 emp::web::Document doc{"target"};
 
 class NFAnimator : public emp::web::Animate {
+public:
+    emp::web::Canvas canvas{500, 500, "canvas"};
 
+    NFAnimator() {
+        doc << canvas;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                canvas.Rect(i * 50, j * 50, 50, 50, "white", "black");
+            }
+        }
+    }
+    void FindNeighbor(int x, int y) {
+        
+        canvas.Rect(x, y, 50, 50, "black", "black");
+        if(y+50 < 500) {
+            canvas.Rect(x, y+50, 50, 50, "red", "black");
+        }
+        if(x+50 < 500) {
+            canvas.Rect(x+50, y, 50, 50, "red", "black");
+        }
+        if(x-50 > 0){
+            canvas.Rect(x-50, y, 50, 50, "red", "black");
+        }
+        if(y-50 > 0) {
+            canvas.Rect(x, y-50, 50, 50, "red", "black");
+        }
+        
+    }
+    
 };
 
 NFAnimator animator;
 
-int main() {
+int main()
+{
     animator.Step();
+    animator.FindNeighbor(200, 200);
+    animator.FindNeighbor(0, 200);
+    animator.FindNeighbor(200, 0);
+    animator.FindNeighbor(400, 300);
+    animator.FindNeighbor(200, 400);
+    animator.FindNeighbor(300, 0);
 }
